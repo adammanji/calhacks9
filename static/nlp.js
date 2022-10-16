7aa6dad55f49e4cf6b05e6862be744d71a77d914
Adam Manji <adam.manji@berkeley.edu>
2022-10-16 06:43:39 -0700

    Added 'Clear' and test data, Adjusted for Oscillator, new model trained

--- static/nlp.js
+++ static/nlp.js
@@ -1,34 +1,62 @@
 function process_nlp(phrase) {
 
-    $.post('/nlp', {'phrase': phrase}, function(data, status, jqXHR) {
+    currentModel = ""
+    // comment this out when we have an actual way to track this
+
+    $.post('/nlp', {'phrase': phrase, 'model': currentModel}, function(data, status, jqXHR) {
+
+    /*  POSSIBLE ERRORS
+
+        no number - cannot find a number
+        command not valid on oscillator - increase or decrease on oscillator
+        no match for oscillator set - none of the oscillator set phrases 
+
+    */
 
         if (data['error']) {
 
             e = data['error'];
             
             if (e == 'no number') {
-
                 alert("no number given!");
-                return;
-
             }
+
+            else if (e == 'command not valid on oscillator') {
+                alert(e);
+            }
+
+            else if (e == 'no match for oscillator set') {
+                alert(e);
+            }
+
+            return;
 
         }
 
         passed_info = data['info'];
         command = data['command'];
 
-        // passed_info has certain variables based on command
+        /*
+        
+        passed_info has certain variables based on command
 
-            // Create - ['model': 'pendulum']
-            // Increase, Decrease, Set - ['variable': 'height', 'amount': 0.0]
-            // Plot - ['variables': ['velocity', 'time']]
+            Create - ['model': 'pendulum']
+            Increase, Decrease, Set - ['variable': 'height', 'amount': 0.0]
+            Plot - ['variables': ['velocity', 'time']]
+            Clear - []
 
-            // variables = ['mass', 'length', 'height', 'gravity', 'angle', 'angular velocity']
-            // plot_variables = variables.copy() + ['potential', 'kinetic', 'time']
-            // models = ['block on a ramp', 'pendulum', 'quantum harmonic oscillator', 'mobius strip']
+            IF MODEL IS OSCILLATOR, Increase and Decrease return errors
+                Set returns ['which': 'ground state'] (one of the key phrases)
 
-        // get back commands to interpret, run those commands w troy's code
+            variables = ['mass', 'length', 'height', 'gravity', 'angle', 'angular velocity']
+            plot_variables = variables.copy() + ['potential', 'kinetic', 'time']
+            models = ['block on a ramp', 'pendulum', 'quantum harmonic oscillator', 'mobius strip']
+            oscillator_key_phrases = ['ground state', 'add some second stationary state', 'add a bit of first excited state',
+                'give me a random linear combination of stationary states', 'coherent state']
+
+        get back commands to interpret, run those commands w troy's code 
+        
+        */
 
     });
 
