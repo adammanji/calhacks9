989e4f33b35c2fe60b649629dcd125ed44e7ee31
Nidhi Nayak <nidhinayak@berkeley.edu>
2022-10-16 12:09:54 -0700

    pushing for button t roy boy

--- static/show-image.js
+++ static/show-image.js
@@ -1,4 +1,11 @@
 function setImageVisible(id, visible) {
     var img = document.getElementById(id);
-    img.style.visibility = (visible ? 'visibility' : 'hidden');
+    if (visible == "true"){
+        img.style.visibility = "visible"; 
+    }
+    else {
+        img.style.visibility = "hidden"; 
+    }
 }
+
+
